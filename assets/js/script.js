80d95b3bbd11b0744aa9aa8849401e80ed1d35ce
erikaosterbur <erikaosterbur@gmail.com>
2021-09-30 20:47:25 -0500

    search bar returns weather results and displays on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,10 @@
 
-// search button has event listener
-    // fetches data from API - one request or two
-    // add div for current weather & populate it
-    // add div of divs for five-day forecast
-    // add classes for UV Index
-    // add little pictures of weather
-    // create button for searched cities with event listener that sends back to query for search bar
-
-
 var APIkey = "13587ddc1a93abaf22f594f3491aed1e";
 var cityInput = $("#city");
 var searchBtn = $("#searchBtn");
 var searchedCitiesContainer = $("#searched-cities-container");
-var cityContainer = $("city-container");
+var cityContainer = $("#city-container");
 var fiveDayForecastContainer = $("#five-day-forecast-container");
-
 
 searchBtn.on("click", function formSubmitHandler(event){
     event.preventDefault();
@@ -28,14 +18,16 @@ searchBtn.on("click", function formSubmitHandler(event){
     }
 })
 
+    // gets lattitude & longitude coordinates and plugs them into the next function
   function getCity(city){
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
     fetch(queryURL)
     .then(function(response){
       if (response.ok) {
         response.json().then(function(data){
-            console.log(data, city);
-          displayCity(data, city);
+            var lat = data.coord.lat;
+            var long = data.coord.lon;
+            getCityCoords(lat, long);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -46,17 +38,50 @@ searchBtn.on("click", function formSubmitHandler(event){
     });
 };
 
-function displayCity(data, city){
-    if (city.length === 0) {
-        cityContainer.textContent = 'No city found.';
-        return;
+ // uses entered city coordinates to generate API data
+ function getCityCoords(lat, long){
+    var geoURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + APIkey;
+    fetch(geoURL)
+    .then( function(response){
+      if (response.ok) {
+          response.json().then(function(data){
+              console.log(data);
+              displayCityData(data);
+          })
+      } else {
+          alert('Error: ' + response.statusText);
       }
+    })
+    .catch(function(error){
+      alert('Unable to connect to Weather Dashboard');
+    });
+}
 
-    //append city name & date to container
+function displayCityData(data){
+    //displays hidden container
+    cityContainer.show();
 
-    console.log(data.main.temp);
+    //displays searched for city title and date
+    var cityTitle = cityInput.val().trim();
+    var date = moment().format('l');
+    $("#city-title").empty().prepend(cityTitle + " -- " + date);
+
+    //inserts weather icon 
+    var iconCode = data.current.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    $('#wicon').attr('src', iconURL);
     
+    //gets current weather info from API
+    var temp = data.current.temp;
+    var wind = data.current.wind_speed;
+    var humidity = data.current.humidity;
+    var uvi = data.current.uvi;
 
+    //appends weather info to the page
+    $("#uv-index").empty().append("UV Index: " + uvi);
+    $("#humidity").empty().append("Humidity: " + humidity + "%");
+    $("#wind").empty().append("Wind: " + wind + " MPH");
+    $("#temp").empty().append("Temp: " + temp + "°F");
 }
 
 
